894253f6e180ef48bc9b6c4731faa4674e7af0fd
Hew <github@mhs.fastmail.com>
2024-06-14 17:34:59 -0400

    No need to wait during boot

--- bin/start.sh
+++ bin/start.sh
@@ -1,28 +1,17 @@
 #!/bin/sh
 
-# Function to kill background processes when exiting
 cleanup() {
     echo "Caught SIGINT signal. Shutting down..."
-    # Kill the background process
     kill $WP_NOW_PID
-    # You may also want to kill the wp-scripts process here, if necessary
-    # kill $WP_SCRIPTS_PID
     exit 0
 }
 
 # Trap SIGINT (Ctrl+C) and SIGTERM signals to run the cleanup function
 trap 'cleanup' SIGINT SIGTERM
 
-# Run wp-now start in the background
+# Run wp-now in the background
 wp-now start --blueprint='.wp-playground/wp-playground.json' --skip-browser &
 WP_NOW_PID=$!
 
-# Wait a bit for wp-now to initialize before starting the next command
-sleep 2
-
 # Run wp-scripts start in the foreground
 wp-scripts start 
-WP_SCRIPTS_PID=$!
-
-# Wait for wp-scripts process to finish
-wait $WP_SCRIPTS_PID
